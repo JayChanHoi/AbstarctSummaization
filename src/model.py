221a33fe1bd1fb919df258058e9ab4e86e8cafa5
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2020-10-05 17:46:58 +0800

    commit to update

---
+++ src/model.py
@@ -0,0 +1,11 @@
+import transformers
+import torch
+import torch.nn as nn
+
+class AbstractSummarizationModel():
+    def __init__(self,):
+        super(AbstractSummarizationModel, self).__init__()
+        pass
+
+    def forward(self):
+        pass
